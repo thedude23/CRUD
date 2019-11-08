6bdca5e0ed53e6ff451e7474dea869345ce2ae49
TK <42032201+thedude23@users.noreply.github.com>
2019-11-08 14:12:34 +0100

    backend finished

---
+++ resources/js/ThemeMode.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="thememode" :class="(mode === 'dark') ? 'dark' : 'light'">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'thememode',
+    data() {
+        mode: 'dark'
+    },
+    components: {
+        
+    }    
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box; 
+    font-family: 'Roboto', sans-serif;
+}
+
+.thememode {
+    width: 100vw;
+    min-height: 100vh;
+    background: #f3f3f3;
+    color: #15202b;
+}
+
+.dark {
+    background: #192734;
+    color: #f3f3f3;
+}
+</style>
